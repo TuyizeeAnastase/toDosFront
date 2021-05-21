b8b64dad80f80aaa4a7170596d33b5f1f19a06ad
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-05-21 13:42:52 +0200

    first commit

---
+++ todos/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <div>toDoApp</div>
+      <div>logged in toDo aPP</div>
+      <div>Account</div>
+    </div>
+    <div> 
+       <h1>teh Todos</h1>
+       <div>
+       <h1>Title</h1>
+       <p>description</p>
+       <p>time</p>
+       </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
